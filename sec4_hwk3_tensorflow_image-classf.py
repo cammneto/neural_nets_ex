35eac7d3f798ffee223c5127260a4e9bff31f917
cammneto <cammneto@gmail.com>
2022-06-18 22:31:43 -0300

    modified:   sec4_hwk3_tensorflow_image-classf.py

--- sec4_hwk3_tensorflow_image-classf.py
+++ sec4_hwk3_tensorflow_image-classf.py
@@ -1,4 +1,3 @@
-import tensorflow
 import matplotlib.pyplot as plt
 import numpy as np
 from keras.models import Sequential
@@ -8,9 +7,9 @@ from keras.datasets import fashion_mnist
 
 (x_train, y_train), (x_test, y_test)= fashion_mnist.load_data()
 
-#plt.imshow(x_train[0], cmap='gray')
-#plt.title('Class: ' + str(y_train[0]))
-#plt.show()
+i = np.random.randint(0, len(X_train))
+plt.imshow(X_train[i], cmap = 'gray')
+plt.title('Class ' + str(y_train[i]))
 
 x_train = x_train.reshape(60000, 28*28)
 x_test = x_test.reshape(10000, 28*28)
@@ -18,8 +17,8 @@ x_test = x_test.reshape(10000, 28*28)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 
-x_train = x_train/255
-x_test = x_test/255
+x_train /= 255
+x_test /= 255
 
 y_train = np_utils.to_categorical(y_train)
 
@@ -31,7 +30,7 @@ network.add(Dense(units=397, activation='relu'))
 network.add(Dense(units=10, activation='softmax'))
 network.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-history = network.fit(x_train, y_train, batch_size=128, epochs=60)
+history = network.fit(x_train, y_train, batch_size=128, epochs=40)
 
 plt.plot(history.history['loss'])
 plt.show()
